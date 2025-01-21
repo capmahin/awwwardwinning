d6a937d2267d0fb0cf88207c7a23e76e81491785
capmahin <mahinulhuque1996@gmail.com>
2025-01-21 13:25:15 +0600

    render the title in span

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -7,6 +7,10 @@ const Button = ({ title, id, rightIcon, leftIcon, containerClass }) => {
       className={`relative z-10 py-3 overflow-hidden text-black rounded-full cursor-pointer group w-fit bg-violet-50 px-7 ${containerClass}`}
     >
       {leftIcon}
+
+      <span className="relative overflow-hidden text-xs uppercase incline-flex font-general">
+        <div>{title}</div>
+      </span>
     </button>
   );
 };
