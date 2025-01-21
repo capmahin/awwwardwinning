6ce5cbd2a73187ebf451d563792020dc1a47badf
capmahin <mahinulhuque1996@gmail.com>
2025-01-21 13:19:56 +0600

    add daymanic class

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ const Button = ({ title, id, rightIcon, leftIcon, containerClass }) => {
   return (
     <button
       id={id}
-      className="relative z-10 py-3 overflow-hidden text-black rounded-full cursor-pointer group w-fit bg-violet-50 px-7"
+      className={`relative z-10 py-3 overflow-hidden text-black rounded-full cursor-pointer group w-fit bg-violet-50 px-7 ${containerClass}`}
     >
       {leftIcon}
     </button>
