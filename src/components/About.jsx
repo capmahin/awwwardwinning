b281058b0de5a2085ee34c271ba17bd49e4e9a67
capmahin <mahinulhuque1996@gmail.com>
2025-01-23 14:41:52 +0600

    add 2 screen

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div id="about" className="w-screen min-h-screen">
+      About
+    </div>
+  );
 };
 
 export default About;
