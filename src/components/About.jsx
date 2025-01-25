7f2c3168b298cbc15f8b8b794e8621d0cf1e194e
capmahin <mahinulhuque1996@gmail.com>
2025-01-25 18:13:36 +0600

    add another h2

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const About = () => {
   return (
     <div id="about" className="w-screen min-h-screen">
-      <div className="relative flex flex-col items-center gap-5 mb-8 mt-36"></div>
+      <div className="relative flex flex-col items-center gap-5 mb-8 mt-36">
+        <h2 className="font-general text-sm uppercase md:text-[10px]"></h2>
+      </div>
     </div>
   );
 };
