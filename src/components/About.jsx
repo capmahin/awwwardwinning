3badeeb7857e1826c284aca98e017970d74a353b
capmahin <mahinulhuque1996@gmail.com>
2025-01-29 22:04:36 +0600

    add

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,26 @@
+import { useGSAP } from "@gsap/react";
 import React from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
 
 const About = () => {
+  useGSAP(() => {
+    const clipAnimation = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#clip",
+        start: "center center",
+        end: "+=800 center",
+        scrub: 0.5,
+        pin: true,
+        pinSpacing: true
+      }
+    });
+    clipAnimation.to(".mask-clip-path", {
+      width: "100vw",
+      height: "100vh",
+      borderRadius: 0
+    });
+  });
   return (
     <div id="about" className="w-screen min-h-screen">
       <div className="relative flex flex-col items-center gap-5 mb-8 mt-36">
