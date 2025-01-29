a3819339761904275d6284fd752448c855f45616
capmahin <mahinulhuque1996@gmail.com>
2025-01-29 13:10:11 +0600

    add more

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,16 @@ const About = () => {
   return (
     <div id="about" className="w-screen min-h-screen">
       <div className="relative flex flex-col items-center gap-5 mb-8 mt-36">
-        <h2 className="font-general text-sm uppercase md:text-[10px]"></h2>
+        <h2 className="font-general text-sm uppercase md:text-[10px]">
+          Welcome to Zentry
+        </h2>
+        <div
+          className="mt-5 text-center text-4xl uppercase 
+        leading-[0.8] md:text-[6rem]"
+        >
+          Disc<b>o</b>ver the world&apos;s <br /> largest shared <b>a</b>
+          dventure
+        </div>
       </div>
     </div>
   );
