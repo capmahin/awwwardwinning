2e4d8cff0b589aceaf7ae0413c9563058a00a2df
capmahin <mahinulhuque1996@gmail.com>
2025-01-29 13:13:50 +0600

    add div and p tag

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,6 +14,13 @@ const About = () => {
           Disc<b>o</b>ver the world&apos;s <br /> largest shared <b>a</b>
           dventure
         </div>
+        <div className="about-subtext">
+          <p>The Game of Games begins—your life, now an epic MMORPG</p>
+          <p className="text-gray-500">
+            Zentry unites every player from countless games and platforms, both
+            digital and physical, into a unified Play Economy
+          </p>
+        </div>
       </div>
     </div>
   );
