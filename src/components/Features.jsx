14ff29c98c93cfb8d7b02c873478f05f1eda3717
capmahin <mahinulhuque1996@gmail.com>
2025-02-28 19:04:19 +0600

    add more div with padding

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Features = () => {
-  return <div>Features</div>;
+  return (
+    <section className="bg-black pb-52">
+      <div className="container px-3 mx-auto md:px-10">
+        <div className="px-5 py-32"></div>
+      </div>
+    </section>
+  );
 };
 
 export default Features;
