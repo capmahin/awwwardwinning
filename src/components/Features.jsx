c26cb60b1acb4eab2084f45ef7f2ed210694b5cb
capmahin <mahinulhuque1996@gmail.com>
2025-02-28 19:06:37 +0600

    add p tag

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -4,7 +4,16 @@ const Features = () => {
   return (
     <section className="bg-black pb-52">
       <div className="container px-3 mx-auto md:px-10">
-        <div className="px-5 py-32"></div>
+        <div className="px-5 py-32">
+          <p className="text-lg font-circular-web text-blue-50">
+            Into the Metagame Layer
+          </p>
+        </div>
+        <p className="max-w-md text-lg opacity-50 font-circular-web text-blue-50">
+          Immerse yourself in a rich and ever-expanding universe where a vibrant
+          array of products converge into an interconnected overlay experience
+          on your world.
+        </p>
       </div>
     </section>
   );
