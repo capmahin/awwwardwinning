36a004cde2be2dc72d96a61fe6a9777a72a1542c
capmahin <mahinulhuque1996@gmail.com>
2025-03-02 13:00:43 +0600

    addd

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Story = () => {
-  return <div>Story</div>;
+  return (
+    <section id="story" className="w-screen bg-black min-h-dvh text-blue-50">
+      <div className="flex flex-col items-center py-10 pb-20 size-full">
+        <p className="font-general text-sm uppercase md:text-[10px]">
+          the multiversal ip world
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default Story;
