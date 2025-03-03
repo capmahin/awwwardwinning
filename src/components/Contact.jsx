f235b951557c28bba913aa9093d649a59cb40430
capmahin <mahinulhuque1996@gmail.com>
2025-03-03 18:06:52 +0600

    add clipclass and src

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 
+const ImageClipBox = ({ src, clipClass }) => (
+  <div className={clipClass}>
+    <img src={src} />
+  </div>
+);
+
 const Contact = () => {
   return (
     <div id="contact" className="w-screen px-10 my-20 min-h-96">
       <div className="relative py-24 bg-black rounded-lg text-blue-50 sm:overflow-hidden">
         <div className="absolute top-0 hidden h-full overflow-hidden -left-20 w-72 sm:block lg:left-20 lg:w-96">
-          <div className="contact-clip-path-1">
-            <img src="img/contact-1.webp" alt="" />
-          </div>
+          <ImageClipBox
+            clipClass="contact-clip-path-1"
+            src="img/contact-1.webp"
+          />
         </div>
       </div>
     </div>
