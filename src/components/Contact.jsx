804d9d1c4fa7e84e7c1bfdf05e25706d0606ba94
capmahin <mahinulhuque1996@gmail.com>
2025-03-04 00:11:59 +0600

    solve

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Button from "./Button";
 
 const ImageClipBox = ({ src, clipClass }) => (
   <div className={clipClass}>
@@ -15,7 +16,17 @@ const Contact = () => {
             clipClass="contact-clip-path-1"
             src="img/contact-1.webp"
           />
+          <ImageClipBox
+            src="/img/contact-2.webp"
+            clipClass="contact-clip-path-2 lg:translate-y-40 translate-y-60"
+          />
         </div>
+        {/* <div className="absolute hidden h-full overflow-hidden -left-20 w-72 sm:block lg:left-20 lg:w-96">
+          <ImageClipBox
+            clipClass="contact-clip-path-1"
+            src="img/contact-2.webp"
+          />
+        </div> */}
         <div className="absolute -top-40 left-20 w-60 sm:top-1/2 md:left-auto md:right-10 lg:top-20 lg:w-80">
           <ImageClipBox
             clipClass="absolute md:scale-125"
@@ -35,6 +46,8 @@ const Contact = () => {
             Let's b<b>u</b>ild the <br /> new era of <br /> g<b>a</b>ming t
             <b>o</b>gether
           </p>
+
+          <Button title="contact us" containerClass="mt-10 cursor-pointer" />
         </div>
       </div>
     </div>
